72c74c4d30afaee459d803608009f2849d46a284
P ollution <bagussatrionugroho@gmail.com>
2020-09-10 03:43:44 +0000

    Getter Setter

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ const mobilsaya = new Car("Xenia","Sedan");
 const mobilorang = new Car("Honda","Bus");
 mobilsaya.info();
 mobilorang.info();
+mobilsaya.nama="ganti";
+mobilsaya.info();
+
+//==GETTER SETTER
+console.log("///////////////////");
+class Car1{
+  constructor(nama="Mobil",warna="Transparan"){
+    this.nama = nama;
+    this._warna = warna;
+  }
+  set warna(value){
+    console.log(`warna mobilnya dirubah`);
+    this._warna=value;
+  }
+  get warna(){
+    return `Warnanya ${this._warna}`;
+  }
+  info(){
+    return this.nama;
+    // console.log(this._warna);
+  }
+}
+
+const mobil1 = new Car1;
+console.log(mobil1.warna);
+mobil1.warna="Merah";
+console.log(mobil1.warna);
